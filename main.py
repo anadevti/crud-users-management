8bcac29eb6e0972bca38e7712a6d53922f9c7331
jorgelucas-rm <jorgelucas.rm@outlook.com>
2024-09-10 15:18:27 -0300

    Feat:Criando um banco de dados e uma camada de serviço para aplicação

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+from database import db
 from routes.home import home_route
 from routes.client import client_route
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave de segurança.'
+
+conexao = 'mysql+pymysql://root:admin@localhost:3306/database'
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(home_route)
 app.register_blueprint(client_route, url_prefix='/clients')
